766de6c64ddf6d199e64714bcda67c268fde403e
DBugMode <manojshenoy@ManojShenoysMBP.fios-router.home>
2015-12-06 10:26:22 -0500

    Projects

---
+++ build/IMMSlackerClient.build/Release-iphoneos/IMMSlackerClient.build/Objects-normal/arm64/IMMSlackerClient.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/Macintosh\ SSD1/Users/manojshenoy/CSCI-E-71_IMM_Slacker_Client_iOS/IMMSlackerClient/IMMSlackerClient.m \
+  /Volumes/Macintosh\ SSD1/Users/manojshenoy/CSCI-E-71_IMM_Slacker_Client_iOS/IMMSlackerClient/IMMSlackerClient.h
